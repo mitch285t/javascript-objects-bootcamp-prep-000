8543b31fdd2b5f05dc23da7d8db03775f8712dd5
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-29 18:44:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,7 @@ var playlist = {'kanye west': "Power"};
 
 function updatePlaylist(object,key, value){
   return playlist['Slowdive'] = "Allison"
+  return playlist[]
 }
 
 function removeFromPlaylist(object, key){
